924400cbde5b87d40faf58968cdb697d2d168940
apoorva-bh <89023113+apoorva-bh@users.noreply.github.com>
2023-02-02 20:08:10 +0530

    updating sample.java

---
+++ sample.java
@@ -0,0 +1,22 @@
+class Sample
+{
+public static void main(String[] a)
+{
+char c = 'A';
+switch(c)
+{
+case 'A':
+System.out.println("A");
+}
+break;
+case 'B':
+{
+System.out.println("B");
+}
+break;
+default:
+{
+System.out.println("Invalid");
+}
+}
+}
